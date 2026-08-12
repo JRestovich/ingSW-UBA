94854541f61840dd02b399c27586f2a4ffaa573d
JRestovich <joaquinrestovich96@mail.com>
2026-08-12 19:07:41 -0300

    first implementation of pwm output

--- ingSw/Core/Src/main.c
+++ ingSw/Core/Src/main.c
@@ -94,6 +94,8 @@ int main(void)
   MX_USART2_UART_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  uint32_t period = __HAL_TIM_GET_AUTORELOAD(&htim1);
 
   /* USER CODE END 2 */
 
@@ -104,6 +106,17 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (period * 10U) / 100U);
+    HAL_Delay(30000);
+
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (period * 25U) / 100U);
+    HAL_Delay(30000);
+
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (period * 75U) / 100U);
+    HAL_Delay(30000);
+
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, period);
+    HAL_Delay(30000);
   }
   /* USER CODE END 3 */
 }
