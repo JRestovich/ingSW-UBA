002d1e05da614212ec9311e4660c81ef347e6b27
JRestovich <joaquinrestovich96@mail.com>
2026-08-19 21:09:54 -0300

    add active object base

---
+++ ingSw/Core/Inc/active_object.h
@@ -0,0 +1,30 @@
+#ifndef ACTIVE_OBJECT_H_
+#define ACTIVE_OBJECT_H_
+
+/********************** CPP guard ********************************************/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/********************** inclusions *******************************************/
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "task.h"
+
+/********************** typedef **********************************************/
+typedef struct
+{
+    TaskHandle_t h_task;
+    QueueHandle_t h_queue;
+    char queue_txt[15];
+    char task_txt[15];
+} active_object_t;
+
+/********************** End of CPP guard *************************************/
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ACTIVE_OBJECT_H_ */
+
+/********************** end of file ******************************************/
