a3d013fc53227f1c690999af9df5b0e0c7a8b62c
JRestovich <joaquinrestovich96@mail.com>
2026-08-23 18:39:21 -0300

    update protocol

--- ingSw/Services/dispatcher/protocol.h
+++ ingSw/Services/dispatcher/protocol.h
@@ -13,12 +13,17 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include <stdbool.h>
+#include <stdint.h>
 #include "FreeRTOS.h"
 #include "queue.h"
 
 /* Private includes ----------------------------------------------------------*/
-#define MSG_SIZE 64
-#define PAYLOAD_MAX_SIZE 60
+#define PROTOCOL_LENGTH_FIELD_LEN 2U
+#define PROTOCOL_HEADER_LEN       5U // Includes PROTOCOL_LENGTH_FIELD_LEN
+#define MSG_SIZE                  64U
+#define PAYLOAD_MAX_SIZE          (MSG_SIZE - PROTOCOL_HEADER_LEN)
+#define PROTOCOL_START_BYTE '!'
 
 typedef enum {
     SUBSYSTEM_LED_RGB,
@@ -28,18 +33,32 @@ typedef enum {
 
 typedef struct
 {
-    char startByte;
+    uint8_t startByte;
     uint8_t subsystem;
     uint8_t cmd;
-    uint8_t msg_size;
-	uint8_t	payload[PAYLOAD_MAX_SIZE];
+    uint8_t payload_size[PROTOCOL_LENGTH_FIELD_LEN];
+    uint8_t payload[PAYLOAD_MAX_SIZE];
 } protocol_msg_t;
 
 typedef union
 {
-    protocol_msg_t msg;
-    uint8_t rawData[MSG_SIZE];    
+    protocol_msg_t frame;
+    uint8_t rawData[MSG_SIZE];
 } protocolData_u;
+
+static inline bool PROTOCOL_payloadSizeToUint8(
+    const uint8_t payload_size[PROTOCOL_LENGTH_FIELD_LEN], uint8_t *size)
+{
+    if (payload_size == NULL || size == NULL ||
+        payload_size[0] < (uint8_t)'0' || payload_size[0] > (uint8_t)'9' ||
+        payload_size[1] < (uint8_t)'0' || payload_size[1] > (uint8_t)'9') {
+        return false;
+    }
+
+    *size = (uint8_t)(((payload_size[0] - (uint8_t)'0') * 10U) +
+                      (payload_size[1] - (uint8_t)'0'));
+    return true;
+}
 
 typedef struct
 {
